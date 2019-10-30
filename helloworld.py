e5e024948542ad75b4363f0d901c2fb91087b69d
cmaceachern <caelmac12@gmail.com>
2019-10-30 10:49:53 -0400

    This is Testing GitHub

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,59 @@
-print("Hello World")
+print("Cael ;)")
+
+
+def backwardslist(array):
+    for word in range(len(array) - 1, -1, -1):
+        print(array[word], end="")
+    print("\n")
+    return
+    """ Takes in a list and returns the list backwards"""
+
+
+def min(array):
+    return min(array)
+
+    """ Returns the lowest number in an list of numbers"""
+
+
+def firsthalfsum(array):
+    sum = 0
+    for number in range(len(array) / 2, 1, 1):
+        sum += array
+    """Returns the sum of the first half of the list.
+        ***IF THE LIST HAS AN ODD NUMBER OF ELEMENTS, split the middle element in
+        half and add it to the sum.
+        """
+
+
+def divisibleby(array, divisor):
+    """ Returns each element divisible by the paramater 'divisor'
+
+    """
+
+
+def max(array):
+    """ Returns the highest number in a list of numbers """
+
+
+def avg(array):
+    """ Returns the average of a list of numbers"""
+
+
+def suprise():
+    print("hello, this is Cael")
+
+
+################################
+####    BONUS FUNCTION       ###
+################################
+def gcd(array):
+    """ Returns the greatest common Divisor of a list of numbers """
+    """ Greatest Common Divisor is the greatest number that each number in the list is 
+        divisible by. 
+        EXAMPLE: [500, 50, 20] Greatest Common Divisor = 10
+                 [18, 30, 42]  Greatest Common Divisor = 6
+                 [33, 66, 99, 101] Greatest Common Divisor = 1
+
+                 """
+
+
